fc8948961bc55ef3f6633945d8d60ff330272938
ExzorS <ilya.trohimik@gmail.com>
2022-11-03 22:48:38 +0300

    fix: class Ship and add class Game

--- sea-battle.py
+++ sea-battle.py
@@ -26,7 +26,7 @@ class BoardWrongsShipException(BoardException):
     pass
 
 class Ship:
-    def ___init__(self, bow, l, o):
+    def __init__(self, bow, l, o):
         self.bow = bow
         self.l = l
         self.o = o
@@ -171,3 +171,82 @@ class User(Player):
             x, y = int(x), int(y)
 
             return Dot(x - 1, y - 1)
+
+class Game:
+    def __init__(self, size = 6):
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
+
+    def try_board(self):
+        lens = [3, 2, 1, 1, 1, 1]
+        board = Board(size = self.size)
+        attempts = 0
+        for l in lens:
+            while True:
+                attempts += 1
+                if attempts > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), l, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except BoardWrongsShipException:
+                    pass
+        board.begin()
+        return board
+
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.try_board()
+        return board
+
+    def greet(self):
+        print('-------------------')
+        print('  Приветсвуем вас  ')
+        print('      в игре       ')
+        print('    морской бой    ')
+        print('-------------------')
+        print(' формат ввода: x y ')
+        print(' x - номер строки  ')
+        print(' y - номер столбца ')
+
+    def loop(self):
+        num = 0
+        while True:
+            print('-' * 20)
+            print('Доска Игрока:')
+            print(self.us.board)
+            print('-' * 20)
+            print('Доска компьютера:')
+            print(self.ai.board)
+            print('-' * 20)
+            if num % 2 == 0:
+                print('Ходит Игрок!')
+                repeat = self.us.move()
+            else:
+                print('Ходит компьютер!')
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.count == 7:
+                print('-' * 20)
+                print('Вы победили!')
+                break
+
+            if self.us.board.count == 7:
+                print('-' * 20)
+                print('Компьютер победил!')
+                break
+            num += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+
